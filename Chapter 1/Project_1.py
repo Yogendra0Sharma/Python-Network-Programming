e4ca244e2f9945d826283626c6c732a0e9e57380
Yogendra Sharma <eh.yogendra@gmail.com>
2017-06-28 16:12:20 +0530

    added Project Related to Chapter 1

---
+++ Chapter 1/Project_1.py
@@ -0,0 +1,21 @@
+#Author : Yogendra Sharma
+#Date   : 28/06/2017
+
+# Take port number from user and show service belong to given port.
+
+# importing python socket module
+
+import socket
+
+
+def ShowServiceByPort(port):
+    try:
+        service = socket.getservbyport(port)
+        print("The Port '{0}' Belongs to '{1}' Service".format(port,service))
+    except OSError:
+        print("Oops!  That was no valid number.  Try again...")
+
+if __name__ == "__main__":
+    port = int(input("enter a port number:"))
+    ShowServiceByPort(port)
+    
